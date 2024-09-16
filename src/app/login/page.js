c4497285ccd88f0fa6e4fe45963c7ad3f8c88679
Barshon <barshonweb@gmail.com>
2024-09-16 18:39:09 +0600

    router navigation hook

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Login = () => {
     return (
         <div className='h-screen bg-black text-white'>
-            login page  
+            login page   
         </div>
     );
 };

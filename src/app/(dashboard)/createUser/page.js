6bea2a71211d4889699b558f2315401421ff726e
Barshon <barshonweb@gmail.com>
2024-09-18 22:31:09 +0600

    group routing

---
+++ src/app/(dashboard)/createUser/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const createUser = () => {
+    return (
+        <div className='bg-black h-screen text-white'>
+            create user page
+        </div>
+    );
+};
+
+export default createUser;
---
+++ src/app/(dashboard)/dashboard/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const dashboard = () => {
+    return (
+        <div className='h-screen bg-black text-white'>
+            dashboard page
+        </div>
+    );
+};
+
+export default dashboard;
